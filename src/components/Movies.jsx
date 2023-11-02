0e3c2cfbd75d32f2e0bb6906fbaae13425dd3f53
Tobias Foit <tobias.foit@gmail.com>
2023-11-02 15:15:42 +0000

    br

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,7 +2,11 @@ export default function Movies({ movieTitle, movieImg }) {
   return (
     <div>
       <>
-        <h2 id="movietitle">Recommended Movie: {movieTitle}</h2>
+        <h2 id="movietitle">
+          Recommended Movie:
+          <br />
+          {movieTitle}
+        </h2>
         <img id="movieImg" src={movieImg} alt={movieTitle} />
       </>
     </div>
