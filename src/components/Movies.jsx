698bc22839675eaaac88c254b39b748d07920e15
Tobias Foit <tobias.foit@gmail.com>
2023-11-02 14:25:06 +0000

    mini

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,7 +3,7 @@ export default function Movies({ movies }) {
     <div>
       <>
         <p>
-          <img src={JSON.stringify(movies.data)} alt="" />
+          <img src={JSON.stringify(movies)} alt="" />
           {JSON.stringify(movies.data)}
         </p>
       </>
