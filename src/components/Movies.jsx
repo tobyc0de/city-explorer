8576a0f9b244c1bc694ba134340ad0347a08e205
Tobias Foit <tobias.foit@gmail.com>
2023-11-02 15:07:37 +0000

    text

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,7 +2,7 @@ export default function Movies({ movieTitle, movieImg }) {
   return (
     <div>
       <>
-        <h2>{movieTitle}</h2>
+        <h2>Movie Recommendation: {movieTitle}</h2>
         <img src={movieImg} alt="tete" />
       </>
     </div>
