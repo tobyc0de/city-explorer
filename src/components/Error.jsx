c0931b96f50526eb72c3d9e3f7b6fac026fbcaf2
Tobias Foit <tobias.foit@gmail.com>
2023-11-03 13:50:43 +0000

    css

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,6 +5,10 @@ export default function Error({ search, error }) {
         Your search for {JSON.stringify(search)} failed, whoops. The API said "
         {error}". Probably the weather API failed or something :)
       </p>
+      <img
+        src="https://media.tenor.com/gMrW5JhB0TUAAAAd/oops-jim-carrey.gif"
+        alt="whoopsie"
+      />
     </>
   );
 }
